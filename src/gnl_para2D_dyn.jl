b3944ac50b8592ded6116a89779feae46eeb6237
Shagun Agarwal <s.agarwal@tudelft.nl>
2024-01-09 19:04:56 +0100

    Dynamic coded. It diverges after a few time-steps.

--- src/gnl_para2D_dyn.jl
+++ src/gnl_para2D_dyn.jl
@@ -16,25 +16,32 @@ using LineSearches: BackTracking
 using LineSearches: Static
 using LinearAlgebra
 using CSV
+using Printf
 
 
 # File properties
-filename = datadir("sims","mem")
+filename = datadir("sims","run","mem")
 
 
 # Material properties
 E = 1e4
-ρₘ = 1
+ρc = 200 #Kg/m3
 L = 1 #m
 A = 0.01
 μₘ = 0.5*E
 @show EA = E*A
-@show m = ρₘ * A
+@show m = ρc * A
 @show L
+
+# Time Parameters
+t0 = 0.0
+simΔt = 0.0001
+simT = 2
+outΔt = 0.002
 
 
 # Parameter Domain
-nx = 1000
+nx = 100
 domain = (0, L)
 partition = (nx)
 model = CartesianDiscreteModel(domain, partition)
@@ -94,9 +101,12 @@ reffe = ReferenceFE(lagrangian,
 ## Define Trial Fnc
 # ---------------------Start---------------------
 # Dirichlet BC
-g1(x) = VectorValue(0.0, 0.0)
+g1(x, t::Real) = VectorValue(0.0, 0.0)
+g1(t::Real) = x -> g1(x, t)
+g2(x, t::Real) = VectorValue(-0.0001*sin(2*pi/1.0*t), 0.0)
+g2(t::Real) = x -> g2(x, t)
 
-U = TrialFESpace(Ψu, [g1,g1])
+U = TransientTrialFESpace(Ψu, [g2,g1])
 # ----------------------End----------------------
 
 
@@ -146,76 +156,89 @@ stressσ(u) = ( FΓ(u) ⋅ stressS(u) ⋅ FΓ(u)' ) / sΛ(u)
 FExth = interpolate_everywhere(VectorValue(0.0, -2000), Ψu)
 
 
+
 ## Weak form
 # ---------------------Start---------------------
-res(u, ψu) =  
-  ∫( ( ∇X_Dir(ψu) ⊙ stressK(u) - ( ψu ⋅ FExth ) )*((J ⊙ J).^0.5) )dΩ 
+res(t, u, ψu) =  
+  ∫( 
+    ( 
+      (ψu ⋅ ∂tt(u)) * ρc +
+      - ∇X_Dir(ψu) ⊙ stressK(u) + 
+      ( ψu ⋅ FExth ) 
+    )*((J ⊙ J).^0.5) 
+  )dΩ 
 
+
+op_AD = TransientFEOperator(res, U, Ψu; order=2)
+# ----------------------End----------------------
+
+
+## Initial solution
+# ---------------------Start---------------------
+
+# Initial solution
+da = wload(datadir("sims","mem_sol.jld2"))
+uf = FEFunction( Ψu, da["uh"] )
+
+# Ua(r) = VectorValue(0.0, -0.001*sin(π*r[1]))
+# U0 = interpolate_everywhere(VectorValue(0.0, 0.0), U(t0))
+
+U0 = interpolate_everywhere(uf, U(t0))
+
+U0t = interpolate_everywhere(VectorValue(0.0, 0.0), U(t0))
+U0tt = interpolate_everywhere(VectorValue(0.0, 0.0), U(t0))
 # ----------------------End----------------------
 
 
 ## Solver
 # ---------------------Start---------------------
 
-# op = FEOperator(res, U, Ψu)
-# uh = solve(op)
-
-# # Initial solution
-# @show U_sag = 0.01*L
-# αU2 = (U_sag*(2/αX1/L)^2, αX1*L/2, -U_sag)
-# @show αU2
-# fx_U(r) = 0.0
-# fy_U(r) = αU2[1] * (fx_X(r)-αX2[2]).^2 + αU2[3]
-# Ua(r) = VectorValue(fx_U(r), fy_U(r))
-# # Ua(r) = VectorValue(0.0, -0.00001)
-# U0 = interpolate_everywhere(Ua, U)
-
-# Initial solution
-fx_U(r) = 0.0#sin(π*r[1])
-fy_U(r) = -0.01*sin(π*r[1])
-Ua(r) = VectorValue(fx_U(r), fy_U(r))
-# Ua(r) = VectorValue(0.0, -0.00001)
-U0 = interpolate_everywhere(Ua, U)
-
-op = FEOperator(res, U, Ψu)
-
-# nls = NewtonRaphsonSolver(LUSolver(), 1e-5, 100)
-
 nls = NLSolver(show_trace=true, 
   method=:newton, linesearch=Static(), 
-  iterations=100, ftol = 1e-8)
+  iterations=100, ftol = 1e-6)
 
-# nls = NLSolver(show_trace=true, 
-#   method=:anderson,  iterations=10)
+ode_solver = GeneralizedAlpha(nls, simΔt, 0.0)    
 
-# solver = FESolver(nls)
-
-#uh = solve(nls, op)
-(uh, cache) = solve!(U0, nls, op)
-
-
-xNew = Xh + uh
-
-writevtk(Ω, filename*"_referenceDomain",
-  cellfields=["XOrig"=>X, "XNew"=>xNew, "uh"=>uh, 
-    "ETang"=>ETang(uh), "sigma"=>stressσ(uh) ])
-
-
-LNew = sum(∫( sqrt∘(JNew(uh) ⊙ JNew(uh)) )dΩ)
-println("Length Original = ", LOrig )  
-println("Length New = ", LNew )  
-println("Ratio = ", LNew/LOrig )  
-
-# Potential energy calculation
-pot = A/2 * sum(∫( ETang(uh) ⊙ stressS(uh) * ((J ⊙ J).^0.5) )dΩ)
-println("Stored potential energy = ", pot)
-
+solnht = solve(ode_solver, op_AD, (U0,U0t,U0tt), t0, simT) 
 # ----------------------End----------------------
 
-data = Dict(
-  "uh" => get_free_dof_values(uh)
-)
 
-wsave(filename*"_sol.jld2", data)
+createpvd(filename*"_tSol") do pvd
+  uh = U0
+  tval = @sprintf("%5.6f",t0)                
+  println("Time : $tval")
+  tprt = @sprintf("%d",floor(Int64,t0*1000))                        
+
+  xNew = X + uh
+
+  pvd[t0] = createvtk(Ω,    
+      filename*"_tSol_$tprt"*".vtu",
+      cellfields=["XOrig"=>X, "XNew"=>xNew, "uh"=>uh, 
+      "ETang"=>ETang(uh), "sigma"=>stressσ(uh) ])
+end
+
+
+
+# Execute
+outMod = floor(Int64,outΔt/simΔt);
+createpvd(filename*"_tSol", append=true) do pvd    
+  cnt=0
+  for (uh, t) in solnht                       
+    cnt = cnt+1    
+    tval = @sprintf("%5.6f",t)                
+    println("Time : $tval")
+    tprt = @sprintf("%d",floor(Int64,t*1000))                        
+
+    if(cnt%outMod != 0) 
+      continue
+    end
+    xNew = X + uh
+
+    pvd[t] = createvtk(Ω,    
+      filename*"_tSol_$tprt"*".vtu",
+      cellfields=["XOrig"=>X, "XNew"=>xNew, "uh"=>uh, 
+      "ETang"=>ETang(uh), "sigma"=>stressσ(uh) ])
+  end
+end    
 
 # end
