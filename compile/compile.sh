7bd354c8ae36ab7b4aede61d0eca8e3177f1e19d
Oriol Colomes <oriol.colomes@gmail.com>
2024-02-07 21:41:22 +0100

    modified compile.sh

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name="compile"
+#SBATCH -p compute
+#SBATCH -t 00:50:00
+#SBATCH -n 1
+#SBATCH -o stdout
+#SBATCH -e stderr
+
 source ./modules.sh
 
-julia --project=.. -e "using Pkg; Pkg.instantiate(); Pkg.precompile()"
-julia --project=.. -O3 --check-bounds=no --color=yes -e "include("./warmup.jl")"
+srun julia --project=.. -e "using Pkg; Pkg.instantiate(); Pkg.precompile()"
+srun julia --project=.. -O3 --check-bounds=no --color=yes -e 'include("warmup.jl")'
