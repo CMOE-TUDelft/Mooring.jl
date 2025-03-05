10b48b4732ea3b7c36fbdcd2bd541df948a88450
Shagun Agarwal <s.agarwal@tudelft.nl>
2025-03-05 15:21:37 +0100

    Trying @quickactivate Mooring instead of @quickactivate Mooring.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,10 @@
 using DrWatson, Test
-@quickactivate "Mooring.jl"
+@quickactivate "Mooring"
 
 # Here you include files using `srcdir`
 # include(srcdir("file.jl"))
 
 # Run test suite
 println("Starting tests")
-ti = time()
 
-@testset "Mooring.jl tests" begin
-    @test 1 == 1
-end
-
-ti = time() - ti
-println("\nTest took total time of:")
-println(round(ti/60, digits = 3), " minutes")
+@time @testset "Demo" begin include(projectdir("test","demo_test.jl")) end
