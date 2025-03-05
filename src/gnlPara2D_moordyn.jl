53b98b25a18f574fdf46f92bb79b4c747a6be87f
Shagun Agarwal <s.agarwal@tudelft.nl>
2025-03-05 16:27:09 +0100

    Removed DrWatson from gnlPara2D_moordyn.jl

--- src/gnlPara2D_moordyn.jl
+++ src/gnlPara2D_moordyn.jl
@@ -1,9 +1,5 @@
 module gnlPara2D
 
-using DrWatson
-@quickactivate "Mooring"
-
-
 using Revise
 using Gridap
 using Gridap.Algebra
@@ -34,7 +30,7 @@ using Mooring.FairLeadMotion
 
 
 
-include(srcdir("subroutines","testParams.jl"))
+include(joinpath("subroutines","testParams.jl"))
 
 
 
@@ -61,11 +57,11 @@ function main(params)
   # ----------------------End----------------------  
 
 
-  ## tagsave git commit
-  gitCommit = Dict(:message=>"git info")
-  gitCommit = tag!(gitCommit; commit_message=true)
-  printTer("[SHOW] git commit"); showTer(gitCommit)
-  printTer()
+  # ## tagsave git commit
+  # gitCommit = Dict(:message=>"git info")
+  # gitCommit = tag!(gitCommit; commit_message=true)
+  # printTer("[SHOW] git commit"); showTer(gitCommit)
+  # printTer()
 
   @unpack ρw = params #Kg/m3 density of water      
 
