3ce6684b5a7a3842f89c195236d638215cac010d
Oriol Colomes <oriol.colomes@gmail.com>
2025-03-05 14:43:12 +0100

    Added deployment

--- docs/make.jl
+++ docs/make.jl
@@ -2,11 +2,22 @@ using Documenter, Mooring
 
 pages = [
     "Home" => "index.md",
-    "Mooring.StressLinear" => "StressLinear.md"
+    "StressLinear" => "StressLinear.md"
 ]
 
 makedocs(
     sitename = "Mooring.jl",
+    format = Documenter.HTML(
+    size_threshold=nothing
+    ),
+    modules = [Mooring],
+    doctest = false,
+    warnonly = [:cross_references,:missing_docs],
+    checkdocs = :exports,
     remotes = nothing,
     pages = pages
+)
+
+deploydocs(
+  repo = "github.com/CMOE-TUDelft/Mooring.jl.git",
 )
