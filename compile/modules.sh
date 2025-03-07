4aa5b321cc2769a1ca3775b485856885d6f9b42e
Oriol Colomes <oriol.colomes@gmail.com>
2025-03-07 08:34:03 +0100

    Moved scripts, models, logs, and data to new repo: MooringTests.jl

--- compile/modules.sh
+++
@@ -1,3 +0,0 @@
-module load 2023r1 openmpi
-
-export UCX_WARN_UNUSED_ENV_VARS=n
--- compile/warmup.jl
+++
@@ -1,9 +0,0 @@
-module Warmup
-using Mooring
-using DrWatson
-@quickactivate "Mooring.jl"
-
-params = Mooring.gnl_simple_params(nx=3,sampling_points=[0.5,0.75])
-Mooring.main_static(params)
-Mooring.main_dynamic(params)
-end
