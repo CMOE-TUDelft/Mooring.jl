0e39bdd8eed908abdfe96e7452e18e554fdaed16
Oriol Colomes <oriol.colomes@gmail.com>
2025-03-15 13:58:06 +0100

    Improved documentation in TDC

--- src/TangentialDiffCalculus.jl
+++ src/TangentialDiffCalculus.jl
@@ -7,36 +7,42 @@ export J, G, Q, ∇ₓΓdir, FΓ, P
 export j, g, q, Λ
 
 """
-J (Jacobian operator)
+    J(X::CellField)
+
+**Jacobian operator**
 
 This function returns the Jacobian operator of a given coordinate map field `X`. We use the transpose as 
 the gradient operator is defined as `∇(X)=∇×X` in Gridap. The Jacobian operator is defined as:
 
 ```math
-\\mathbf{J}(\\mathbf{X}) = ∇_r(\\mathbf{X}) = \\frac{∂X_i}{∂r_j}
+mathbf{J}(mathbf{X}) = ∇_r(mathbf{X}) = rac{∂X_i}{∂r_j}
 ```
 Note that the dimensions of the Jacobian operator are `n×1`, where `n` is the dimension of the physical space.
 """
 J(X::CellField) = ∇(X)'
 
 """
-G (Metric tensor)
+    G(J)
+
+**Metric tensor**
 
 This function returns the metric tensor of a given coordinate map field `X`. The metric tensor is defined as:
-\$ \\mathbf{G} = \\mathbf{J}^T \\cdot \\mathbf{J} \$, with \$ \\mathbf{J} \$ the [J](@ref) operator of the map field `X`.
+$ \mathbf{G} = \mathbf{J}^T \cdot \mathbf{J} $, with $ \mathbf{J} $ the [J](@ref) operator of the map field `X`.
 
 Note that the dimensions of the metric tensor are `1×1`.
 """
 G(J) = J'⋅J
 
 """
-Q (Transformation matrix)
+    Q(J)
 
-This function returns the transformation matrix \$ Q \$ of a given coordinate map field `X`. 
+**Transformation matrix**
+
+This function returns the transformation matrix $ Q $ of a given coordinate map field `X`. 
 The transformation matrix is defined as: 
 
 ```math
-\\mathbf{Q} = \\mathbf{J}\\cdot\\mathbf{G}^{-1}
+\mathbf{Q} = \mathbf{J}\cdot\mathbf{G}^{-1}
 ```
 
 Note that the dimensions of the transformation matrix are `n×1`, where `n` is the dimension of the physical space.
@@ -44,7 +50,9 @@ Note that the dimensions of the transformation matrix are `n×1`, where `n` is t
 Q(J) = J⋅inv(G(J))
 
 """
-∇ₓΓdir (Tangential gradient)
+    ∇ₓΓdir(u::CellField,X::CellField)
+
+**Tangential gradient**
 
 This function returns the tangential gradient of a given vector field `u`. The tangential gradient is defined as:
 
@@ -55,10 +63,18 @@ This function returns the tangential gradient of a given vector field `u`. The t
 Note that the dimensions of the tangential gradient are `n×n`, where `n` is the dimension of the physical space.
 """
 ∇ₓΓdir(u::CellField,X::CellField) = ∇(u)'⋅(Q(J(X))')
+
+"""
+    ∇ₓΓdir(∇u::TensorValue,J::TensorValue)
+
+[∇ₓΓdir](@ref) (Tangential gradient)
+"""
 ∇ₓΓdir(∇u::TensorValue,J::TensorValue) = ∇u'⋅(Q(J)')
 
 """
-FΓ (line deformation gradient)
+    FΓ(u::CellField,X::CellField)
+
+**Line deformation gradient**
 
 This function returns the line deformation gradient for a given displacement field `u`, and 
 a map field `X`. The line deformation gradient is defined as:
@@ -74,10 +90,18 @@ function FΓ(u::CellField,X::CellField)
     I = one∘(∇u)
     return ∇u + I
 end
+
+"""
+    FΓ(∇xΓdir_u::TensorValue)
+
+[∇xΓdir](@ref) (Line deformation gradient)
+"""
 FΓ(∇xΓdir_u::TensorValue) = ∇xΓdir_u + one(∇xΓdir_u)
 
 """
-P (Projection operator)
+    P(J)
+
+**Projection operator**
 
 This function returns the projection operator of a given vector field `u` onto the tangent space of the map field `X`.
 The projection operator is defined as:
@@ -91,7 +115,9 @@ Note that the dimensions of the projection operator are `n×n`, where `n` is the
 P(J) = (J⋅J')/(J⊙J)
 
 """
-j (Jacobian operator in the physical space)
+    j(FΓ,J)
+
+**Jacobian operator in the physical space**
 
 This function returns the Jacobian determinant in the physical space for a given line deformation gradient `FΓ` and
 Jacobian operator `J`. The Jacobian determinant in the physical space is defined as:
@@ -105,7 +131,9 @@ Note that the dimensions of the Jacobian determinant in the physical space are `
 j(FΓ,J) = FΓ'⋅J
 
 """
-g (Metric tensor in the physical space)
+    g(j)
+
+**Metric tensor in the physical space**
 
 This function returns the metric tensor in the physical space as:
 
@@ -118,7 +146,9 @@ Note that the dimensions of the metric tensor in the physical space are `1×1`.
 g(j) = j'⋅j
 
 """
-q (Transformation matrix in the physical space)
+    q(j)
+
+**Transformation matrix in the physical space**
 
 This function returns the transformation matrix in the physical space as:
 
@@ -131,12 +161,14 @@ Note that the dimensions of the transformation matrix in the physical space are 
 q(j) = j⋅inv(g(j))
 
 """
-Λ (Stretch)
+    Λ(j,J)
+
+**Line stretch**
 
 This function returns the stretch of a given line. The stretch is defined as:
 
 ```math
-Λ = \\frac{\\det(g)}{\\det(G)}
+Λ = \frac{\det(g)}{\det(G)}
 ```
 
 where `g` is the metric tensor in the physical space, and `G` is the metric tensor in the reference space.
