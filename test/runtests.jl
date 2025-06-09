20f6f6c72bc7d000b93a3d0a76ee301c5de273d3
Oriol Colomes <oriol.colomes@gmail.com>
2025-06-09 11:28:24 +0200

    Restructuring folders

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,11 @@ using Test
 # Run test suite
 println("Starting tests")
 
-@time @testset "EnvironmentalConditions" begin include(joinpath("EnvironmentalConditionsTests.jl")) end
-@time @testset "SeaBed" begin include(joinpath("SeaBedTests.jl")) end
-@time @testset "Drag" begin include(joinpath("DragTests.jl")) end
-@time @testset "PointMotion" begin include(joinpath("PointMotionTests.jl")) end
-@time @testset "TangentialDiffCalculus" begin include(joinpath("TangentialDiffCalculusTests.jl")) end
-@time @testset "Materials" begin include(joinpath("MaterialsTests.jl")) end
+# Physics tests
+@time @testset "EnvironmentalConditions" begin include(joinpath("Physics/EnvironmentalConditionsTests.jl")) end
+@time @testset "SeaBed" begin include(joinpath("Physics/SeaBedTests.jl")) end
+@time @testset "Drag" begin include(joinpath("Physics/DragTests.jl")) end
+@time @testset "PointMotion" begin include(joinpath("Physics/PointMotionTests.jl")) end
+@time @testset "TangentialDiffCalculus" begin include(joinpath("Physics/TangentialDiffCalculusTests.jl")) end
+@time @testset "Materials" begin include(joinpath("Physics/MaterialsTests.jl")) end
 # @time @testset "Demo" begin include(joinpath("demo_test.jl")) end
