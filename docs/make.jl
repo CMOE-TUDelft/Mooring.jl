298fc635bc58192a5f79c992f5ed1f3ff9b6a45c
Oriol Colomes <oriol.colomes@gmail.com>
2025-09-13 20:54:02 +0200

    Changing docs structure

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,41 @@
 using Documenter, Mooring
 
+user_pages = []
+
+developer_pages = []
+
+entities_pages = [
+  "API/Entities/MooringPoint.md",
+  "API/Entities/MooringSegment.md",
+  "API/Entities/MooringLine.md",
+]
+
+geometry_pages = [
+  "API/Geometry/MooringDiscreteModel.md",
+]
+
+physics_pages = [
+  "API/Physics/EnvironmentalConditions.md",
+  "API/Physics/SeaBed.md",
+  "API/Physics/Drag.md",
+  "API/Physics/PointMotion.md",
+  "API/Physics/TangentialDiffCalculus.md",
+]
+
+api_pages = [ 
+  "API/API.md", 
+  "Input/Output"=>"API/IO/ParameterHandler.md",
+  "Entities" => entities_pages,
+  "Geometry" => geometry_pages,
+  "Physics" => physics_pages
+]
+
 pages = [
     "Home" => "index.md",
-    "Environmental Conditions" => "Physics/EnvironmentalConditions.md",
-    "Sea Bed" => "Physics/SeaBed.md",
-    "Drag" => "Physics/Drag.md",
-    "Point Motion" => "Physics/PointMotion.md",
-    "Tangential Differential Calculus" => "Physics/TangentialDiffCalculus.md",
+    "Getting Started" => "GettingStarted.md",
+    "User Guide" => "UserGuide.md",
+    "Developer Guide" => "DeveloperGuide.md",
+    "API" => "API/API.md",
 ]
 
 makedocs(
