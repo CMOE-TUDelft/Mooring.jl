266cf8d32aa804f2988ff9a8acb76e39f79b67b9
Oriol Colomes <oriol.colomes@gmail.com>
2025-09-15 14:36:47 +0200

    fixes

--- src/IO/ParameterHandler.jl
+++ src/IO/ParameterHandler.jl
@@ -153,11 +153,15 @@ DragParameters
   Ca_t::Float64 = 0.0
   Cfd_n::Float64 = 0.0
   Cfd_t::Float64 = 0.0
-  function DragParameters(tag="default", dragType="NoDrag", ρw=0.0, nd=0.0, od=0.0, id=0.0, AStr=0.0,
-    Cd_n=0.0, Cd_t=0.0, dd_n=0.0, dd_t=0.0,
-    Ca_n=0.0, Ca_t=0.0, Cfd_n=0.0, Cfd_t=0.0)
-    new(tag, dragType, ρw, nd, od, id, AStr, Cd_n, Cd_t, dd_n, dd_t, Ca_n, Ca_t, Cfd_n, Cfd_t)
-  end
+end
+
+"""
+DragParameters constructor
+"""
+function DragParameters(tag="default_drag", dragType="NoDrag", ρw=0.0, nd=0.0, od=0.0, id=0.0, AStr=0.0,
+  Cd_n=0.0, Cd_t=0.0, dd_n=0.0, dd_t=0.0,
+  Ca_n=0.0, Ca_t=0.0, Cfd_n=0.0, Cfd_t=0.0)
+  DragParameters(tag, dragType, ρw, nd, od, id, AStr, Cd_n, Cd_t, dd_n, dd_t, Ca_n, Ca_t, Cfd_n, Cfd_t)
 end
 
 """
