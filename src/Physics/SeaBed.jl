99940d80027f98f894dab0a0cac6f93b2d4431fd
Oriol Colomes <oriol.colomes@gmail.com>
2025-11-17 09:02:57 +0100

    cleaning commented code in SeaBed.jl

--- src/Physics/SeaBed.jl
+++ src/Physics/SeaBed.jl
@@ -68,63 +68,6 @@ function ramp_linear(params::SeaBedParameters, excursion::Real)
     return 0.0
 end
 
-
-# """
-# sea_bed_force
-
-# This function computes the force exerted by the sea bed on the mooring line. It considers the
-# excursion of the line into the sea bed, the velocity of the line, and the properties of the sea bed.
-# The function is defined as:
-
-# ```math
-# \\alpha_1 * w + \\alpha_2 * k * s_\\Lambda * ( x_z - \\beta_1 * v_z - \\beta_2 * v_z * \\abs(v_z) )
-# ```
-
-# Input:
-# - `params::SeaBedParameters`: Sea bed parameters
-# - `X::VectorValue`: Position of the line
-# - `QTr::TensorValue`: Transformation matrix \$ Q^T \$
-# - `T1s::TensorValue`: Stress tensor
-# - `T1m::Real`: Maximum stress
-# - `u::VectorValue`: Displacement of the line
-# - `∇u::TensorValue`: Gradient of the displacement
-# - `v::VectorValue`: Velocity of the line
-
-# Output:
-# - `VectorValue`: Force exerted by the sea bed at a given point in the line
-# """
-# function sea_bed_force(params::SeaBedParameters, X::VectorValue, 
-#     QTr::TensorValue, T1s::VectorValue, T1m::Real, 
-#     u::VectorValue, ∇u::TensorValue, v::VectorValue)
-    
-#     # Define local variables
-#     local excursion, ramp_factor
-#     local FΓ, t1s, t1m2, sΛ        
-    
-#     excursion = VectorValue(0.0,-1.0) ⋅ (X + u) # assumes flat bed in the x-y plane
-#     ramp_factor = ramp_tanh(params, excursion)
-#     half_ramp_factor = min(1.0, ramp_factor)
-    
-#     vz = VectorValue(0.0, 1.0) ⋅ v # velocity in the z direction
-    
-#     FΓ = ( ∇u' ⋅ QTr ) + TensorValue(1.0,0.0,0.0,1.0)
-#     t1s = FΓ ⋅ T1s
-#     t1m2 = t1s ⋅ t1s    
-    
-#     sΛ = (t1m2.^0.5) / T1m # Stretch along the line
-    
-#     # params.cnstz = params.kn * params.od / params.A
-#     @unpack still_weight, cnstz, linear_damping_factor, quadratic_damping_factor = params
-    
-#     return half_ramp_factor * still_weight  + 
-#     ramp_factor * cnstz * sΛ * ( 
-#     excursion +
-#     -linear_damping_factor * vz +
-#     -quadratic_damping_factor * vz * abs(vz) 
-#     )
-    
-# end
-
 """
 sea_bed_force
 
